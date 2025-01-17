9123faec0c2d65e5b0d7614bbfca63b1524a6a16
Tanush Chetan <22tchetan@heckgrammar.co.uk>
2025-01-17 23:15:39 +0000

    better

--- HowToUseGitHubClassroom/Program.cs
+++ HowToUseGitHubClassroom/Program.cs
@@ -21,6 +21,11 @@
             // Output to the console the variable
             // Output to the console Hello World
             // Test your program and add the evidence to this repository
+
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+            Console.WriteLine("Hello World");
         }
     }
 }
